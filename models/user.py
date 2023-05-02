03da133e661859cdf6705344fcb3cc2d6b00348e
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2023-05-02 23:19:21 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
+"""
+Review Class from Models Module
+"""
+import os
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 
-class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+class Review(BaseModel, Base):
+    """Review class handles all application reviews"""
+    if storage_type == "db":
+        __tablename__ = 'reviews'
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+        place_id = ''
+        user_id = ''
+        text = ''
